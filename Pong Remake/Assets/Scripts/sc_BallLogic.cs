79d3089ab5545163d67778a9302dc98b99aff571
Jake Meyer <JakeGKMeyer@hotmail.com>
2022-08-28 20:54:36 +1000

    Ball Bounce Logic fix
    Fixed up the ball bouncing logic so that it works smoother, more consistent and more like pong bounces

--- Pong Remake/Assets/Scripts/sc_BallLogic.cs
+++ Pong Remake/Assets/Scripts/sc_BallLogic.cs
@@ -2,8 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class sc_BallLogic : MonoBehaviour
 {
+    private enum PaddlePosition
+    {
+        TOP,
+        MIDDLE,
+        BOTTOM
+    }
+
     [SerializeField] private sc_GameManager GameManager = sc_GameManager.instance;
 
     //Single Player Ball Logic
@@ -17,11 +25,8 @@ public class sc_BallLogic : MonoBehaviour
 
     [Header("Directional Properties")]
 
-    [SerializeField] private Vector3 OldPosition;
-    [SerializeField] private Vector3 NewPosition;
-    [SerializeField] private int TrueDirection;
-
     [SerializeField] private Vector3 Velocity;
+    [SerializeField] private PaddlePosition PaddleHitPosition;
 
     private void Awake()
     {
@@ -43,36 +48,40 @@ public class sc_BallLogic : MonoBehaviour
 
     private void InitialMovement()
     {
-        OldPosition = NewPosition;
-
-        //NewPosition = Random.value > 0.5f ? new Vector3(-GameManager.WorldWidth, Random.Range(-GameManager.WorldHeight, GameManager.WorldHeight)) :
-        //    new Vector3(GameManager.WorldWidth, Random.Range(-GameManager.WorldHeight, GameManager.WorldHeight));
-        // NewPosition = Player.transform.position;
-        //Velocity = (Player.transform.position - transform.position).normalized * MinBallSpeed;
-        //Velocity = Random.value > 0.5f ? new Vector3
-        Vector2 RandomDir = new Vector2(Random.Range(0.3f, 1.0f) * Random.value > 0.5f ? -1 : 1, Random.Range(-1.0f, 1.0f)).normalized * MinBallSpeed;
+        Vector2 RandomDir = new Vector2(Random.Range(0.3f, 1.0f) * Random.value > 0.5f ? -1 : 1, Random.Range(-1.0f, 1.0f)).normalized;
         Velocity = RandomDir;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Velocity * Time.deltaTime);
+        UpdateVelocity();
+
+        //Debugging
+        DisplayDistance();
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             transform.position = new Vector3(0, 0, 0);
-            NewPosition = Player.transform.position;
         }
+    }
+
+    private void UpdateVelocity()
+    {
+        transform.Translate(Velocity * CurrentBallSpeed * Time.deltaTime);
+    }
+
+    private void DisplayDistance()
+    {
+        Debug.DrawLine(transform.position, Player.transform.position, Color.red);
+        Debug.DrawLine(transform.position, AI.transform.position, Color.red);
+        Debug.DrawRay(transform.position, new Vector3(Velocity.x * 10, Velocity.y * 10, 0), Color.blue);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision != null)
         {
-            //Velocity = NewPosition;
-            //Velocity = new Vector3(-Velocity.x, Velocity.y, 0);
-
             bool HorizontalReflection = false;  //Walls
             bool VerticalReflection = false;    //Player/Paddles
 
@@ -89,32 +98,25 @@ public class sc_BallLogic : MonoBehaviour
             {
                 //Velocity = new Vector3(-Velocity.x, Velocity.y, 0);
                 float DirToGo = (transform.position.y - collision.transform.position.y) / (collision.transform.lossyScale.y / 2);
-                TrueDirection = DirToGo < 0 ? Mathf.RoundToInt(DirToGo - GameManager.PaddleOffset) : Mathf.RoundToInt(DirToGo + GameManager.PaddleOffset);
+                DirToGo = DirToGo < 0 ? Mathf.RoundToInt(DirToGo - GameManager.PaddleOffset) : Mathf.RoundToInt(DirToGo + GameManager.PaddleOffset);
+                PaddleHitPosition = DirToGo == -1 ? PaddlePosition.BOTTOM : DirToGo == 1 ? PaddlePosition.TOP : PaddlePosition.MIDDLE;
                 //Check if bounce is in positive velocity (ball going up)
                 if (Mathf.Sign(Velocity.y) > 0)
                 {
-                    float _theta = Mathf.Atan(-Velocity.y / Velocity.x);
-
-                    float _vMaxY = -Velocity.x * Mathf.Sin(_theta) + Velocity.y * Mathf.Cos(_theta);
+                    float _vMaxY = -Velocity.x * Mathf.Sin(Mathf.PI/4) + Velocity.y * Mathf.Cos(Mathf.PI / 4);
 
                     float randY = Random.Range(Velocity.y, _vMaxY);
 
-                    if (_vMaxY < Velocity.y)
+                    switch (PaddleHitPosition)
                     {
-                        Debug.Log("Fuck");
-                    }
-
-                    switch (TrueDirection)
-                    {
-                        case 0:
-                            Velocity = new Vector3(-Velocity.x, Random.Range(Velocity.y - 0.2f, Velocity.y + 0.2f), 0);
+                        case PaddlePosition.MIDDLE:
+                            Velocity = new Vector3(-Velocity.x, Random.Range(0, 0.1f), 0);
                             break;
-
-                        case 1:
+                        case PaddlePosition.TOP:
                             Velocity = randY > 0 ? new Vector2(-Velocity.x, randY) : new Vector2(-Velocity.x, -randY);
                             CurrentBallSpeed++;
                             break;
-                        case -1:
+                        case PaddlePosition.BOTTOM:
                             Velocity = randY > 0 ? new Vector2(-Velocity.x, -randY) : new Vector2(-Velocity.x, randY);
                             CurrentBallSpeed++;
                             break;
@@ -123,28 +125,21 @@ public class sc_BallLogic : MonoBehaviour
                 //Check if bounce is in negative velocity (ball going down)
                 else if (Mathf.Sign(Velocity.y) < 0)
                 {
-                    float _theta = 2 * Mathf.PI - Mathf.Atan(-Velocity.y / Velocity.x);
-
-                    float _vMaxY = -Velocity.x * Mathf.Sin(_theta) + Velocity.y * Mathf.Cos(_theta);
+                    float _vMaxY = -Velocity.x * Mathf.Sin(Mathf.PI / 4) + Velocity.y * Mathf.Cos(Mathf.PI / 4);
 
                     float randY = Random.Range(Velocity.y, _vMaxY);
 
-                    if (_vMaxY < Velocity.y)
+                    switch (PaddleHitPosition)
                     {
-                        Debug.Log("Fuck");
-                    }
-
-                    switch (TrueDirection)
-                    {
-                        case 0:
-                            Velocity = new Vector3(-Velocity.x, Random.Range(Velocity.y - 0.2f, Velocity.y + 0.2f), 0);
+                        case PaddlePosition.MIDDLE:
+                            Velocity = new Vector3(-Velocity.x, Random.Range(-0.1f, 0), 0);
                             break;
 
-                        case 1:
+                        case PaddlePosition.TOP:
                             Velocity = randY > 0 ? new Vector2(-Velocity.x, randY) : new Vector2(-Velocity.x, -randY);
                             CurrentBallSpeed++;
                             break;
-                        case -1:
+                        case PaddlePosition.BOTTOM:
                             Velocity = randY > 0 ? new Vector2(-Velocity.x, -randY) : new Vector2(-Velocity.x, randY);
                             CurrentBallSpeed++;
                             break;
